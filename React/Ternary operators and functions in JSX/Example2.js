b2d7872f7034e9b42acaa6877c1db9615c4e5738
MirageM <imiragesz@gmail.com>
2023-12-10 19:39:55 -0500

    {Math.floor(Math.random() * 10) + 1}

---
+++ React/Ternary operators and functions in JSX/Example2.js
@@ -0,0 +1,9 @@
+function Example2(){
+    return (
+        <div className="heading">
+            <h1>Here's a random number from 0 to 10 :
+                {Math.floor(Math.random() * 10) + 1}
+            </h1>
+        </div>
+    );
+};
