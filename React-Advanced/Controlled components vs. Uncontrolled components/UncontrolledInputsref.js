8860aa33538405d21f452efad85f827384773a22
MirageM <imiragesz@gmail.com>
2024-01-20 14:27:53 -0500

    const inputValue = inputRef.current.value;

---
+++ React-Advanced/Controlled components vs. Uncontrolled components/UncontrolledInputsref.js
@@ -0,0 +1,13 @@
+const Form = () => {
+    const inputRef = useRef(null);
+
+    const handleSubmit = () => {
+        const inputValue = inputRef.current.value;
+        //Do something with the value
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input ref={inputRef} type="text" />
+        </form>
+    );
+};
